d3bce11882a235d0a19bf45a2fafba6593a334c1
ToshieCosed <iquiera@gmail.com>
2017-09-25 19:16:41 -0700

    Initial Actual Upload
    Attempting to fix issues with printing memory address info in the
    SmileHook menu

---
+++ k11_extension/build/svc/GetHandleInfo.d
@@ -0,0 +1,20 @@
+build/svc/GetHandleInfo.o: source/svc/GetHandleInfo.c \
+ include/svc/GetThreadInfo.h include/utils.h include/types.h \
+ include/kernel.h include/svc.h include/globals.h include/config.h \
+ include/memory.h
+
+include/svc/GetThreadInfo.h:
+
+include/utils.h:
+
+include/types.h:
+
+include/kernel.h:
+
+include/svc.h:
+
+include/globals.h:
+
+include/config.h:
+
+include/memory.h:
