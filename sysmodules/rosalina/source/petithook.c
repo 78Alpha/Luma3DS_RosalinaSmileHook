264ca50cbd9105370e4e6f976626b1f582ba0734
ToshieCosed <iquiera@gmail.com>
2017-09-26 18:50:55 -0700

    Stable RamDump automatic
    The ramdump is stable and automatic when you go into the SmileHook menu now. It will generate a file on the SD card.
    If you get a kernel crash in Luma3DS then it's likely caused by the program not being able to create the folder on the root of the sd card which is going to be "/smilehook/memdumps/" and the dumped file generated is called "memdump.bin"
    But yeah this is stable from the looks of it.
    The total dumped size ram I got was 28.4 MB.

--- sysmodules/rosalina/source/petithook.c
+++ sysmodules/rosalina/source/petithook.c
@@ -151,6 +151,8 @@ Result PetitMemHook_DoOrUndoPatches(void)
     s64 startAddress, textTotalRoundedSize, rodataTotalRoundedSize, dataTotalRoundedSize;
     u32 totalSize;
     Handle processHandle;
+    static int memindex = 0;
+    u32 memoffset = 0;
     char buf[65];
 
     Result res = OpenProcessByName("petitcom", &processHandle);
@@ -162,48 +164,26 @@ Result PetitMemHook_DoOrUndoPatches(void)
     Draw_ClearFramebuffer();
     Draw_FlushFramebuffer();
     Draw_Unlock();
+    const u32 destAddress = 0x00100000;
     
-    //dataTotalRoundedSize = 4194304;
-    svcGetProcessInfo(&startAddress, processHandle, 0x10005);
-    res = svcMapProcessMemoryEx(processHandle, 0x00100000, (u32) startAddress, 4194304);
-    u32 my_array[4194304] = {0};
-        
-        
-    u32 thistest[] = {
-        0x000000FF,
-        0x000000FF,
-        0x000000FF,
-        0x000000FF,
-        0x000000FF,
-        0x000000FF,
-    };
+        svcGetProcessInfo(&textTotalRoundedSize, processHandle, 0x10002);
+        svcGetProcessInfo(&rodataTotalRoundedSize, processHandle, 0x10003);
+        svcGetProcessInfo(&dataTotalRoundedSize, processHandle, 0x10004);
+        totalSize = (u32)(textTotalRoundedSize + rodataTotalRoundedSize + dataTotalRoundedSize);
+        svcGetProcessInfo(&startAddress, processHandle, 0x10005);
+        res = svcMapProcessMemoryEx(processHandle, destAddress, (u32)startAddress, totalSize);
+
+    //value_ = ((u32*)(startAddress))[i+0x002FC075 - 0x00100000];
+
+    u32 memoffset = 0;
     
-    
- 
-    
-        //Copy the first 20 32 bit values from SmileBasic ram into my_array!
-    //memcpy(my_array, startAddress, sizeof(my_array-1));
-    u32 value_ = my_array[0];
-    
-    
-    //put the first 8000 bytes of smilebasic's ram into buffer
-    u32 buffer[100] = {0};
-    for (int i=0;i<99;i++){
-      value_ = ((u32*)startAddress)[i];
-      buffer[i] = value_;
-    
-                        }
-    int memoffset = 0;
-    int memindex = 0;
     bool need_update = false;
     //0x10002 to 0x10007 are 
     
     FS_Archive archive;
     FS_ArchiveID archiveId;
     s64 out;
-  
-    
-    
+
     //Trying to write file shit
     u64 total;
     IFile file;
@@ -219,7 +199,6 @@ Result PetitMemHook_DoOrUndoPatches(void)
     archiveId = isSdMode ? ARCHIVE_SDMC : ARCHIVE_NAND_RW;
     Draw_Lock();
     Draw_RestoreFramebuffer();
-
     svcFlushEntireDataCache();
 
     res = FSUSER_OpenArchive(&archive, archiveId, fsMakePath(PATH_EMPTY, ""));
@@ -230,53 +209,29 @@ Result PetitMemHook_DoOrUndoPatches(void)
             res = 0;
         FSUSER_CloseArchive(archive);
     }
-    
-    
-    
-    
 
-
- Result res1, res2, res3;
+        Result res1, res2, res3;
         IFile fileR;
             
             //I think that it's putting the string into the filename instead of the screen?
         sprintf(filename, "/smilehook/memdumps/memdump.bin");
         res1 = IFile_Open(&fileR, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_READ);
         IFile_Close(&fileR);
+        //total = 100; //Try to write 4 MB of ram to dump file
+        TRY(IFile_Open(&file, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_CREATE | FS_OPEN_WRITE));
+        //TRY(IFile_Write(&file, &total, framebufferCache, 3 * 320 * 120, 0));
+    
+      
+      //value_ = ((u32*)startAddress+memoffset+0x002FC075 - 0x00100000)[i];
 
-        //sprintf(filename, "/luma/screenshots/bot_%04u.bmp", filenum);
-        //res2 = IFile_Open(&fileR, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_READ);
-        //IFile_Close(&fileR);
+            //TRY(IFile_Write(&file, &total, buffer, total, 0));
+            TRY(IFile_Write(&file, &total, startAddress, totalSize, 0));
+            TRY(IFile_Close(&file));
 
-        //sprintf(filename, "/luma/screenshots/top_right_%04u.bmp", filenum);
-       // res3 = IFile_Open(&fileR, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_READ);
-       // IFile_Close(&fileR);
-    total = 100; //Try to write 4 MB of ram to dump file
-    TRY(IFile_Open(&file, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_CREATE | FS_OPEN_WRITE));
-    //TRY(IFile_Write(&file, &total, framebufferCache, 3 * 320 * 120, 0));
-    
-    
-    
-    for (int write=0;write<8000;write++){
-    
-    for (int i=0;i<99;i++){
-      value_ = ((u32*)startAddress)[i+memoffset];
-      buffer[i] = value_;
-    
-                        }
-                        memoffset=memoffset+100;
-    
-    
-    TRY(IFile_Write(&file, &total, buffer, total, 0));
-                                        }
-    
-    
-    
-    TRY(IFile_Close(&file));
-    
-    
-     end:
+    end:
     IFile_Close(&file);
+    
+    }
     
     do
     {   
@@ -293,23 +248,9 @@ Result PetitMemHook_DoOrUndoPatches(void)
                 //}
     Draw_Lock();
        
-       
-        //Try to display that ram
-        for ( int a=0; a<24; a++ ) {
-            value_ = ((u32*)startAddress)[a+ memindex];
-             if ( a == 12)  { Draw_DrawFormattedString(10, ((a-1)*10), COLOR_GREEN, "                                                         ");}
-             if ( a == 12)  { Draw_DrawFormattedString(10, ((a-1)*10), COLOR_GREEN, "Address (%p) == (0x%08x).", a+memindex, value_);}
-             else{
-             Draw_DrawFormattedString(10, ((a-1)*10), COLOR_WHITE, "                                                         ");
-             Draw_DrawFormattedString(10, ((a-1)*10), COLOR_WHITE, "Address (%p) == (0x%08x).", a+memindex, value_);
-             
-                 }
-            }
-             
+      
+           // value_ = ((u32*)startAddress)[a];
 
-        //Draw_FlushFramebuffer();
-       
-        
         do{
             //nothing
             Draw_FlushFramebuffer();
@@ -320,199 +261,11 @@ Result PetitMemHook_DoOrUndoPatches(void)
        
         //Draw_ClearFramebuffer();
         
-        Draw_Unlock();
-    }
-    while(!(waitInput() & BUTTON_B) && !terminationRequest);
-}
-        
-        
-        //svcGetProcessInfo(&textTotalRoundedSize, processHandle, 0x10002); // only patch .text + .data
-        //svcGetProcessInfo(&rodataTotalRoundedSize, processHandle, 0x10003);
-        //svcGetProcessInfo(&dataTotalRoundedSize, processHandle, 0x10004);
+       
+    
+    }while(!(waitInput() & BUTTON_B) && !terminationRequest);
 
-        //totalSize = (u32)(textTotalRoundedSize + rodataTotalRoundedSize + dataTotalRoundedSize);
-
-        //svcGetProcessInfo(&startAddress, processHandle, 0x10005);
-        //res = svcMapProcessMemoryEx(processHandle, 0x00100000, (u32) startAddress, totalSize);
-
-        //if(R_SUCCEEDED(res))
-        //{
-            //static const u32 hidOrigRegisterAndValue[] = { 0x1EC46000, 0x4001 };
-            //static const u32 hidOrigCode[] = {
-                //0xE92D4070, // push {r4-r6, lr}
-                //0xE1A05001, // mov  r5, r1
-               // 0xEE1D4F70, // mrc  p15, 0, r4, c13, c0, 3
-               // 0xE3A01801, // mov  r1, #0x10000
-               // 0xE5A41080, // str  r1, [r4,#0x80]!
-           // };
-
-            //static u32 *hidRegPatchOffsets[2];
-            //static u32 *hidPatchJumpLoc;
-
-            //if(petithook_enabled)
-            //{
-               // memcpy(hidRegPatchOffsets[0], &hidOrigRegisterAndValue, sizeof(hidOrigRegisterAndValue));
-                //memcpy(hidRegPatchOffsets[1], &hidOrigRegisterAndValue, sizeof(hidOrigRegisterAndValue));
-                //memcpy(hidPatchJumpLoc, &hidOrigCode, sizeof(hidOrigCode));
-            //}
-           // else
-            //{
-                //u32 hidDataPhys = (u32)PA_FROM_VA_PTR(hidData);
-                //u32 hidCodePhys = (u32)PA_FROM_VA_PTR(&hidCodePatchFunc);
-                //u32 hidHook[] = {
-                    //0xE59F3004, // ldr r3,  [pc, #4]
-                    //0xE59FC004, // ldr r12, [pc, #4]
-                    //0xE12FFF1C, // bx  r12
-                    //hidDataPhys,
-                   // hidCodePhys,
-                //};
-
-               // u32 *off = (u32 *)memsearch((u8 *)0x00100000, &hidOrigRegisterAndValue, totalSize, sizeof(hidOrigRegisterAndValue));
-                //if(off == NULL)
-                //{
-                   // svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-                    //return -1;
-               // }
-
-                //u32 *off2 = (u32 *)memsearch((u8 *)off + sizeof(hidOrigRegisterAndValue), &hidOrigRegisterAndValue, totalSize - ((u32)off - 0x00100000), sizeof(hidOrigRegisterAndValue));
-                //if(off2 == NULL)
-               // {
-                    //svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-                    //return -2;
-                //}
-
-                //u32 *off3 = (u32 *)memsearch((u8 *)0x00100000, &hidOrigCode, totalSize, sizeof(hidOrigCode));
-                //if(off3 == NULL)
-                //{
-                    //svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-                    //return -3;
-               // }
-
-                //hidRegPatchOffsets[0] = off;
-                //hidRegPatchOffsets[1] = off2;
-                //hidPatchJumpLoc = off3;
-
-                //*off = *off2 = hidDataPhys;
-                //memcpy(off3, &hidHook, sizeof(hidHook));
-            //}
-        //}
-
-        //res = svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-    //}
-    //svcCloseHandle(processHandle);
-
-    //res = OpenProcessByName("ir", &processHandle);
-   // if(R_SUCCEEDED(res) && osGetKernelVersion() >= SYSTEM_VERSION(2, 44, 6))
-    //{
-        //svcGetProcessInfo(&textTotalRoundedSize, processHandle, 0x10002); // only patch .text + .data
-        //svcGetProcessInfo(&rodataTotalRoundedSize, processHandle, 0x10003);
-       // svcGetProcessInfo(&dataTotalRoundedSize, processHandle, 0x10004);
-
-       // totalSize = (u32)(textTotalRoundedSize + rodataTotalRoundedSize + dataTotalRoundedSize);
-
-        //svcGetProcessInfo(&startAddress, processHandle, 0x10005);
-        //res = svcMapProcessMemoryEx(processHandle, 0x00100000, (u32) startAddress, totalSize);
-
-        //if(R_SUCCEEDED(res))
-       //{
-            //static bool useOldSyncCode;
-           // static u32 irOrigReadingCode[5] = {
-               // 0xE5940000, // ldr r0, [r4]
-               // 0xE1A01005, // mov r1, r5
-                //0xE3A03005, // mov r3, #5
-               // 0xE3A02011, // mov r2, #17
-               // 0x00000000  // (bl i2c_read_raw goes here)
-           // };
-
-           // static const u32 irOrigWaitSyncCode[] = {
-               // 0xEF000024, // svc 0x24 (WaitSynchronization)
-                //0xE1B01FA0, // movs r1, r0, lsr#31
-                //0xE1A0A000, // mov r10, r0
-            //}, irOrigWaitSyncCodeOld[] = {
-               // 0xE0AC6000, // adc r6, r12, r0
-                //0xE5D70000, // ldrb r0, [r7]
-            //}; // pattern for 8.1
-
-           // static const u32 irOrigCppFlagCode[] = {
-                //0xE3550000, // cmp r5, #0
-                //0xE3A0B080, // mov r11, #0x80
-           // };
-
-           // static u32 *irHookLoc, *irWaitSyncLoc, *irCppFlagLoc;
-
-            //if(petithook_enabled)
-           // {
-               // memcpy(irHookLoc, &irOrigReadingCode, sizeof(irOrigReadingCode));
-                //if(useOldSyncCode)
-                   // memcpy(irWaitSyncLoc, &irOrigWaitSyncCodeOld, sizeof(irOrigWaitSyncCodeOld));
-                //else
-                   // memcpy(irWaitSyncLoc, &irOrigWaitSyncCode, sizeof(irOrigWaitSyncCode));
-               // memcpy(irCppFlagLoc, &irOrigCppFlagCode, sizeof(irOrigCppFlagCode));
-           // }
-           // else
-           // {
-                //u32 irDataPhys = (u32)PA_FROM_VA_PTR(irData);
-                //u32 irCodePhys = (u32)PA_FROM_VA_PTR(&irCodePatchFunc);
-
-               // u32 irHook[] = {
-                    //0xE5940000, // ldr r0, [r4]
-                    //0xE1A01005, // mov r1, r5
-                    //0xE59FC000, // ldr r12, [pc] (actually +8)
-                    ////0xE12FFF3C, // blx r12
-                    //irCodePhys,
-                //};
-
-                //u32 *off = (u32 *)memsearch((u8 *)0x00100000, &irOrigReadingCode, totalSize, sizeof(irOrigReadingCode) - 4);
-                //if(off == NULL)
-                //{
-                    //svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-                    //return -4;
-               // }
-
-                //u32 *off2 = (u32 *)memsearch((u8 *)0x00100000, &irOrigWaitSyncCode, totalSize, sizeof(irOrigWaitSyncCode));
-               // if(off2 == NULL)
-               // {
-                    //off2 = (u32 *)memsearch((u8 *)0x00100000, &irOrigWaitSyncCodeOld, totalSize, sizeof(irOrigWaitSyncCodeOld));
-                   // if(off2 == NULL)
-                   // {
-                        //svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-                       // return -5;
-                   // }
-
-                   // useOldSyncCode = true;
-                //}
-               // else
-                    //useOldSyncCode = false;
-
-               // u32 *off3 = (u32 *)memsearch((u8 *)0x00100000, &irOrigCppFlagCode, totalSize, sizeof(irOrigCppFlagCode));
-               // if(off3 == NULL)
-                //{
-                   // svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-                   // return -6;
-                //}
-
-                //*(void **)(irCodePhys + 8) = decodeARMBranch(off + 4);
-                //*(void **)(irCodePhys + 12) = (void*)irDataPhys;
-
-               // irHookLoc = off;
-               // irWaitSyncLoc = off2;
-                //irCppFlagLoc = off3;
-
-                //irOrigReadingCode[4] = off[4]; // Copy the branch.
-
-               // memcpy(irHookLoc, &irHook, sizeof(irHook));
-
-                // This "NOP"s out a WaitSynchronisation1 (on the event bound to the 'IR' interrupt) or the check of a previous one
-                //*irWaitSyncLoc = 0xE3A00000; // mov r0, #0
-
-                // This NOPs out a flag check in ir:user's CPP emulation
-               // *irCppFlagLoc = 0xE3150000; // tst r5, #0
-            //}
-        //}
-
-        //res = svcUnmapProcessMemoryEx(processHandle, 0x00100000, totalSize);
-    //}
-    //svcCloseHandle(processHandle);
+ Draw_Unlock();//put unmapping process handle stuff here later
 
    
     
