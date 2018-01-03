3a938a96e9f48d88c1fc7dddcdd5286d71a2b0ca
ToshieCosed <iquiera@gmail.com>
2018-01-02 16:48:53 -0800

    SmileNet 2.0 -semi stable
    Just updating.

--- sysmodules/rosalina/include/petithook.h
+++ sysmodules/rosalina/include/petithook.h
@@ -26,34 +26,17 @@
 
 #pragma once
 
-#include <3ds/svc.h>
-#include <3ds/result.h>
-#include "csvc.h"
+#include <3ds/types.h>
+#include "MyThread.h"
+#pragma once
+//#include "common.h"
+#define PAYLOADSIZE    0x400
+#define SOC_ALIGN      0x1000
 
-// For accessing physmem uncached (and directly)
-#define PA_PTR(addr)            (void *)((u32)(addr) | 1 << 31)
 
-#ifndef PA_FROM_VA_PTR
-#define PA_FROM_VA_PTR(addr)    PA_PTR(svcConvertVAToPA((const void *)(addr), false))
-#endif
+extern bool smileNetEnabled;
+extern Handle smileNetThreadStartedEvent;
 
-#define REG32(addr)             (*(vu32 *)(PA_PTR(addr)))
-
-static inline u32 makeARMBranch(const void *src, const void *dst, bool link) // the macros for those are ugly and buggy
-{
-    u32 instrBase = link ? 0xEB000000 : 0xEA000000;
-    u32 off = (u32)((const u8 *)dst - ((const u8 *)src + 8)); // the PC is always two instructions ahead of the one being executed
-
-    return instrBase | ((off >> 2) & 0xFFFFFF);
-}
-
-static inline void *decodeARMBranch(const void *src)
-{
-    u32 instr = *(const u32 *)src;
-    s32 off = (instr & 0xFFFFFF) << 2;
-    off = (off << 6) >> 6; // sign extend
-
-    return (void *)((const u8 *)src + 8 + off);
-}
-
-Result OpenProcessByName(const char *name, Handle *h);
+extern int smileNetStartResult;
+MyThread *smileNetCreateThread(void);
+void smileNetThreadMain(void);
